1c1fd2a3fd8971d04daf1fe46d258871eef2aa59
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-07-16 23:18:48 -0400

    updated link path

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,11 @@ export default function Weather() {
     <div>
       <h1>Hello!</h1>
       <p>
-        <a href="https://github.com/thelunareclipse/weather-react-app">
+        <a
+          href="https://github.com/thelunareclipse/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Github Repository
         </a>
       </p>
