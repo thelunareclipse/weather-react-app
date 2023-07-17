3a656d073a9b4f95b26f2592c409bd0334e2b77a
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-07-16 23:14:19 -0400

    link to Github repository

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <h1>Hello!</h1>;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <p>
+        <a href="https://github.com/thelunareclipse/weather-react-app">
+          Github Repository
+        </a>
+      </p>
+    </div>
+  );
 }
