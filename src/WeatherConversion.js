f2898d36c28cf8922a9796b46d292e8def904856
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-08-06 13:22:42 -0400

    Weather Conversion implementation

---
+++ src/WeatherConversion.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+export default function WeatherConversion(props) {
+  const [unit, setUnit] = useState("celsius");
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  if (unit === "celsius") {
+    return (
+      <span className="WeatherTemp">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">°C</span> |{" "}
+        <span className="unit">
+          <a href="/" onClick={convertToFahrenheit}>
+            °F
+          </a>
+        </span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="WeatherTemp">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToCelsius}>
+            °C
+          </a>
+        </span>{" "}
+        | <span className="unit">°F</span>
+      </span>
+    );
+  }
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherConversion from "./WeatherConversion";
 import "./Weather.css";
 
 export default function WeatherInfo(props) {
@@ -21,10 +22,7 @@ export default function WeatherInfo(props) {
       <div className="row">
         <div className="col-6">
           <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+          <WeatherConversion celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
